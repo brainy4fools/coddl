f0fbadcf3766cc0b1f2e2ecd87a345e4a20e6dd3
ignitedcms <craig@craigs-iMac.lan>
2018-01-03 12:43:27 +0000

    modal dismiss
    added modal dismiss

--- application/views/_calendarfooter.php
+++ application/views/_calendarfooter.php
@@ -1,6 +1,10 @@
 <div class="modal fade" id="modal-form">
     <div class="modal-dialog">
         <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">Bookings</h4>
+            </div>
             <div class="modal-body">
                 <div class="row">
 
